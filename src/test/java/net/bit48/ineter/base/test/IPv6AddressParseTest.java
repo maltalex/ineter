c43028522c748fe941bce74868394aa726965ba5
maltalex <code@bit48.net>
2018-03-21 21:21:23 +0200

    divided tests into the same two packages

--- src/test/java/net/bit48/ineter/base/test/IPv6AddressParseTest.java
+++ src/test/java/net/bit48/ineter/base/test/IPv6AddressParseTest.java
@@ -5,7 +5,7 @@
  * License, v. 2.0. If a copy of the MPL was not distributed with this
  * file, You can obtain one at http://mozilla.org/MPL/2.0/.
  */
-package net.bit48.ineter.test;
+package net.bit48.ineter.base.test;
 
 import static org.junit.Assert.assertFalse;
 import static org.junit.jupiter.api.Assertions.assertEquals;
