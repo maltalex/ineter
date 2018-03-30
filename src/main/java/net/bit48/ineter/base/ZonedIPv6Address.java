6642b9a49bfdfe41c9d3c24fd7428f0178d23f6e
maltalex <code@bit48.net>
2018-03-31 01:20:10 +0300

    ZonedIPv6Address javadoc

--- src/main/java/net/bit48/ineter/base/ZonedIPv6Address.java
+++ src/main/java/net/bit48/ineter/base/ZonedIPv6Address.java
@@ -14,6 +14,16 @@ public class ZonedIPv6Address extends IPv6Address {
 
 	private static final long serialVersionUID = 1L;
 
+	/**
+	 * Build an ZonedIPv6Address from a literal IPv6 with a zone designation
+	 * such as "[fe80::1%2]","fe80::1%eth0", and similar valid forms
+	 *
+	 * @param address
+	 *            with zone
+	 * @return new ZonedIPv6Address instance
+	 * @throws IllegalArgumentException
+	 *             if the provided address is invalid
+	 */
 	public static ZonedIPv6Address of(String address) {
 		IPAddress ip = IPv6Address.of(address);
 		if (!(ip instanceof ZonedIPv6Address)) {
@@ -22,10 +32,32 @@ public class ZonedIPv6Address extends IPv6Address {
 		return (ZonedIPv6Address) ip;
 	}
 
+	/**
+	 * Build a ZonedIPv6Address from two longs and zone - upper and lower 64
+	 * bits in form of longs, and a String zone
+	 *
+	 * @param upper
+	 *            upper 64 bits of the IPv6Address
+	 * @param lower
+	 *            lower 64 bits of the IPv6Address
+	 * @param zone
+	 *            zone String
+	 * @return new IPv6Address instance
+	 */
 	public static ZonedIPv6Address of(long upper, long lower, String zone) {
 		return new ZonedIPv6Address(upper, lower, zone);
 	}
 
+	/**
+	 * Build a ZonedIPv6Address from a 16 byte long big-endian (highest byte
+	 * first) byte array and a zone String
+	 *
+	 * @param bigEndianByteArr
+	 *            16 byte big-endian byte array
+	 * @param zone
+	 *            zone String
+	 * @return new ZoneIPv6Address instance
+	 */
 	public static ZonedIPv6Address of(byte[] bigEndianByteArr, String zone) {
 		verifyArray(bigEndianByteArr);
 		long upper = LongByte.extractLong(bigEndianByteArr, 0);
@@ -33,10 +65,24 @@ public class ZonedIPv6Address extends IPv6Address {
 		return new ZonedIPv6Address(upper, lower, zone);
 	}
 
+	/**
+	 * Build a ZonedIPv6Address from an IPv6Address and a separate zone String
+	 *
+	 * @param address
+	 * @param zone
+	 * @return new ZonedIPv6Address instance
+	 */
 	public static ZonedIPv6Address of(IPv6Address address, String zone) {
 		return of(address.upper, address.lower, zone);
 	}
 
+	/**
+	 * Build a ZonedIPv6Address from an Inet6Address with a scoped interface or
+	 * scope id
+	 *
+	 * @param address
+	 * @return new IPv6Address instance
+	 */
 	public static ZonedIPv6Address of(Inet6Address address) {
 		if (address.getScopedInterface() != null) {
 			return of(address.getAddress(), address.getScopedInterface().getName());
@@ -46,7 +92,18 @@ public class ZonedIPv6Address extends IPv6Address {
 
 	protected final String zone;
 
-	protected ZonedIPv6Address(long upper, long lower, String zone) {
+	/**
+	 * Build a ZonedIPv6Address from two longs an a String - upper and lower 64
+	 * bits in form of longs and a zone
+	 *
+	 * @param upper
+	 *            upper 64 bits of the IPv6Address
+	 * @param lower
+	 *            lower 64 bits of the IPv6Address
+	 * @param zone
+	 *            the IPv6Address
+	 */
+	public ZonedIPv6Address(long upper, long lower, String zone) {
 		super(upper, lower);
 		this.zone = zone;
 	}
@@ -140,11 +197,21 @@ public class ZonedIPv6Address extends IPv6Address {
 		return new ZonedIPv6Address(newUpper, newLower, this.zone);
 	}
 
+	/**
+	 * Does this address have a specific zone?
+	 *
+	 * @return true
+	 */
 	@Override
 	public boolean isZoned() {
 		return true;
 	}
 
+	/**
+	 * Returns the zone this address belongs to
+	 *
+	 * @return the zone
+	 */
 	public String getZone() {
 		return this.zone;
 	}
